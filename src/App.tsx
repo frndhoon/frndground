a2fffa04e25fb31f8dc11bbf2c92ee77755e4777
frndhoon <frndhoon@gmail.com>
2026-02-05 08:20:09 +0900

    vh, dvh, svh, lvh 차이 확인

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { useState } from "react"
+
+function App() {
+  const [vhDialogOpen, setVhDialogOpen] = useState(false)
+  const [dvhDialogOpen, setDvhDialogOpen] = useState(false)
+  const [svhDialogOpen, setSvhDialogOpen] = useState(false)
+  const [lvhDialogOpen, setLvhDialogOpen] = useState(false)
+
+  return (
+    <>
+      <div className="flex gap-2 p-2">
+      <Button onClick={() => setVhDialogOpen(true)}>vh</Button>
+      <Button onClick={() => setDvhDialogOpen(true)}>dvh</Button>
+      <Button onClick={() => setSvhDialogOpen(true)}>svh</Button>  
+      <Button onClick={() => setLvhDialogOpen(true)}>lvh</Button>
+      </div>
+      <Dialog open={vhDialogOpen} onOpenChange={setVhDialogOpen}>
+        <DialogContent className="h-96 max-h-[95vh]">
+          <DialogHeader>
+            <DialogTitle>vh</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={dvhDialogOpen} onOpenChange={setDvhDialogOpen}>
+        <DialogContent className="h-96 max-h-[95dvh]">
+          <DialogHeader>
+            <DialogTitle>dvh</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={svhDialogOpen} onOpenChange={setSvhDialogOpen}>
+        <DialogContent className="h-96 max-h-[95svh]">
+          <DialogHeader>
+            <DialogTitle>svh</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={lvhDialogOpen} onOpenChange={setLvhDialogOpen}>
+        <DialogContent className="h-96 max-h-[95lvh]">
+          <DialogHeader>
+            <DialogTitle>lvh</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
+
+export default App
