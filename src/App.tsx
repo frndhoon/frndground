e2da893fa543952f0cf618bc699753f4336855d6
JeongHoon Woo <frndhoon@gmail.com>
2026-02-05 08:46:46 +0900

    고정 height 값 변경

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,34 @@ function App() {
   return (
     <>
       <div className="flex gap-2 p-2">
-      <Button onClick={() => setVhDialogOpen(true)}>vh</Button>
-      <Button onClick={() => setDvhDialogOpen(true)}>dvh</Button>
-      <Button onClick={() => setSvhDialogOpen(true)}>svh</Button>  
-      <Button onClick={() => setLvhDialogOpen(true)}>lvh</Button>
+        <Button onClick={() => setVhDialogOpen(true)}>vh</Button>
+        <Button onClick={() => setDvhDialogOpen(true)}>dvh</Button>
+        <Button onClick={() => setSvhDialogOpen(true)}>svh</Button>  
+        <Button onClick={() => setLvhDialogOpen(true)}>lvh</Button>
       </div>
       <Dialog open={vhDialogOpen} onOpenChange={setVhDialogOpen}>
-        <DialogContent className="h-96 max-h-[95vh]">
+        <DialogContent className="h-[120rem] max-h-[95vh]">
           <DialogHeader>
             <DialogTitle>vh</DialogTitle>
           </DialogHeader>
         </DialogContent>
       </Dialog>
       <Dialog open={dvhDialogOpen} onOpenChange={setDvhDialogOpen}>
-        <DialogContent className="h-96 max-h-[95dvh]">
+        <DialogContent className="h-[120rem] max-h-[95dvh]">
           <DialogHeader>
             <DialogTitle>dvh</DialogTitle>
           </DialogHeader>
         </DialogContent>
       </Dialog>
       <Dialog open={svhDialogOpen} onOpenChange={setSvhDialogOpen}>
-        <DialogContent className="h-96 max-h-[95svh]">
+        <DialogContent className="h-[120rem] max-h-[95svh]">
           <DialogHeader>
             <DialogTitle>svh</DialogTitle>
           </DialogHeader>
         </DialogContent>
       </Dialog>
       <Dialog open={lvhDialogOpen} onOpenChange={setLvhDialogOpen}>
-        <DialogContent className="h-96 max-h-[95lvh]">
+        <DialogContent className="h-[120rem] max-h-[95lvh]">
           <DialogHeader>
             <DialogTitle>lvh</DialogTitle>
           </DialogHeader>
